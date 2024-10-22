6d51263eed1978d9d9c5463882e6437033293007
Ereh-yeager <s.n.bhatti45@gmail.com>
2024-10-22 10:53:11 +0500

    Updated the project

--- DjangoAuth/DjangoAuth/info.py
+++ DjangoAuth/DjangoAuth/info.py
@@ -1,5 +1,2 @@
-EMAIL_USE_TLS = True
-EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = "temp.use2.12.31.1999@gmail.com"
-EMAIL_HOST_PASSWORD = "Shazimbilal4529"
-EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD = "jbrp uadx zkdk mzlb"
--- DjangoAuth/DjangoAuth/settings.py
+++ DjangoAuth/DjangoAuth/settings.py
@@ -18,14 +18,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # EMAIL_USE_TLS = EMAIL_USE_TLS
 # EMAIL_HOST = EMAIL_HOST
-# EMAIL_HOST_USER = EMAIL_HOST_USER
-# EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
+EMAIL_HOST_USER = EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
 # EMAIL_PORT = EMAIL_PORT
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "temp.use2.12.31.1999@gmail.com"
-EMAIL_HOST_PASSWORD = "jbrp uadx zkdk mzlb"
 EMAIL_PORT = 587
 
 # Quick-start development settings - unsuitable for production
