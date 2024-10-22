d53e232718bbaacd5854f88c9d1c8ad5c69a352f
Shazim <138565709+M-Shazim@users.noreply.github.com>
2024-10-22 11:06:23 +0500

    Update info.py

--- DjangoAuth/DjangoAuth/info.py
+++ DjangoAuth/DjangoAuth/info.py
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-EMAIL_HOST_USER = "temp.use2.12.31.1999@gmail.com"
-EMAIL_HOST_PASSWORD = "jbrp uadx zkdk mzlb"
-=======
 
->>>>>>> 95675c1d768eba5b7f14f0e14c1fd01119f658f2
+EMAIL_HOST_USER = "xxxxxxxxxxxxxxxxxxx"
+EMAIL_HOST_PASSWORD = "xxxxxxxxxxxxxxxxxxxxxx"
+
